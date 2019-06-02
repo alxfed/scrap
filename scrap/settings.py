3a2cd68fdaa4db02f777dd619990746e41494b11
alxfed <alxfed@gmail.com>
2019-06-01 19:13:49 -0500

    project 'scrap'

--- scrap/settings.py
+++ scrap/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'scrap.spiders'
 #USER_AGENT = 'scrap (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
