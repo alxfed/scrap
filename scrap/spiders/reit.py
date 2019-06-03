a61c0a3b32074a2adf471b93d11e97096cfb1b73
Alex Fedotov <alxfed@gmail.com>
2019-06-03 11:14:40 -0500

    Working spiders 'reit' scraping the complete SEC list of REITs

--- scrap/spiders/reit.py
+++ scrap/spiders/reit.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import re
 from scrap.items import SECcompany
 
 
@@ -8,7 +7,7 @@ class ReitSpider(scrapy.Spider):
     name = 'reit'
     allowed_domains = ['sec.gov']
     start_urls = ['https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&SIC=6798&owner=include&match=&start=1&count=100']
-    base_url = ['https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&SIC=6798&owner=include&match=&start=']
+    base_url = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&SIC=6798&owner=include&match=&start='
     page_num = 1
 
     def parse(self, response):
@@ -16,14 +15,9 @@ class ReitSpider(scrapy.Spider):
         """
         EMPTY_PAGE_XPATH = '//div[@class="noCompanyMatch"]/text()'
         COMPANY_LIST_XPATH = '//div[@id="seriesDiv"]/table[@summary="Results"]//tr'
-        # // *[ @ id = "seriesDiv"] / table / tbody / tr[2] / td[1]
         CIK_XPATH = 'td[@scope="row"][1]/a/text()'
         COMPANY_XPATH ='td[@scope="row"][2]/text()'
         STATE_XPATH = 'td[@scope="row"][3]/a/text()'
-
-        response = response.replace(body=re.sub('>\s*<', '><',
-                                                response.body.replace('\n', ''),
-                                                0, re.M))
 
         NO_COMPANY_MATCH = response.xpath(EMPTY_PAGE_XPATH).get()
         if NO_COMPANY_MATCH:
@@ -42,6 +36,6 @@ class ReitSpider(scrapy.Spider):
                     filing['state'] = company.xpath(STATE_XPATH).get()
                     yield filing
 
+            tail_url = r'{}&count=100'.format(str(100*self.page_num))
             self.page_num = self.page_num +1
-            next_page_url = self.base_url + r'{}&count=100'.format(str(100*self.page_num))
-            yield scrapy.Request(url=next_page_url, callback=self.parse)
+            yield scrapy.Request(url=self.base_url + tail_url, callback=self.parse)
