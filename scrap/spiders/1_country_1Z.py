8036b3b9f64c650eac15579624ea21ba6c591aa4
Alex Fedotov <alxfed@gmail.com>
2019-06-07 05:56:14 -0500

    Removing the scaffolds

--- scrap/spiders/1_country_1Z.py
+++ scrap/spiders/1_country_1Z.py
@@ -6,14 +6,14 @@ from scrap.items import SECcompany
 
 
 class ReitSpider(scrapy.Spider):
-    name = '1Z_country'
+    name = '1_country_1Z'
     allowed_domains = ['sec.gov']
-    start_urls = ['https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&State=1Z&owner=include&match=&start=0&count=100&hidefilings=0']
+    start_urls = ['https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&State=1Z&owner=include&match=&start=1&count=100&hidefilings=0']
     base_url = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&State=1Z&owner=include&match=&start='
     page_num = 1
 
     def parse(self, response):
-        """Multipage parse with 100 items on page and incrementable URL
+        """Multipage parse with 100 items per page (starting from #1) and incrementable URL
         """
         EMPTY_PAGE_XPATH = '//div[@class="noCompanyMatch"]/text()'
         COMPANY_LIST_XPATH = '//div[@id="seriesDiv"]/table[@summary="Results"]//tr'
@@ -36,7 +36,7 @@ class ReitSpider(scrapy.Spider):
             for company in company_list:
                 not_header = company.xpath(CIK_XPATH).get()
                 if not_header:
-                    filing = ItemLoader(SECcompany(), company)
+                    filing = ItemLoader(SECcompany(), company)                  #default loader
                     filing.add_xpath('cik',     CIK_XPATH)
                     filing.add_xpath('company', 'td[@scope="row"][2]/text()')
                     filing.add_xpath('state',   'td[@scope="row"][3]/a/text()')
@@ -45,3 +45,5 @@ class ReitSpider(scrapy.Spider):
             tail_url = r'{}&count=100'.format(str(100*self.page_num))
             self.page_num = self.page_num +1
             yield scrapy.Request(url=self.base_url + tail_url, callback=self.parse)
+
+# finished on Jun 7, 2019
